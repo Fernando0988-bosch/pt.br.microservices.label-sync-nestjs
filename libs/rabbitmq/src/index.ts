f8031c75d58653241b16b3d7257fe76d3eec736c
Fernando Nunes <fernando.nunes2@br.bosch.com>
2025-06-26 22:57:06 -0300

    feat(setup): rabbitmq lib progress

--- libs/rabbitmq/src/index.ts
+++ libs/rabbitmq/src/index.ts
@@ -1,5 +1,6 @@
 export * from './rabbitmq.module';
 export * from './rabbitmq.service';
+export * from './rabbitmq-helper.service';
 export * from './decorators';
 export * from './interfaces';
 export * from './publishers';
